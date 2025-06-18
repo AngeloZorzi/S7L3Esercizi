158a340a03a38500ff7e3d4a9342a442bba8355c
AngeloZorzi <astro.drg666@gmail.com>
2025-06-18 14:00:15 +0200

    #1

---
+++ src/main/java/adapter/InfoAdapter.java
@@ -0,0 +1,35 @@
+package adapter;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class InfoAdapter implements DataSource {
+
+    private Info info;
+
+    public InfoAdapter(Info info) {
+        this.info = info;
+    }
+
+    @Override
+    public String getNomeCompleto() {
+        return info.getNome() + " " + info.getCognome();
+    }
+
+    @Override
+    public int getEta() {
+        Date dataNascita = info.getDataDiNascita();
+        if (dataNascita == null) return 0;
+
+        Calendar oggi = Calendar.getInstance();
+        Calendar nascita = Calendar.getInstance();
+        nascita.setTime(dataNascita);
+
+        int eta = oggi.get(Calendar.YEAR) - nascita.get(Calendar.YEAR);
+        if (oggi.get(Calendar.DAY_OF_YEAR) < nascita.get(Calendar.DAY_OF_YEAR)) {
+            eta--;
+        }
+        return eta;
+    }
+}
+
--- src/main/java/adapter/Main.java
+++ src/main/java/adapter/Main.java
@@ -1,4 +1,18 @@
 package adapter;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Main {
+    public static void main(String[] args) {
+        Info info = new Info();
+        info.setNome("Mario");
+        info.setCognome("Rossi");
+        info.setDataDiNascita(new GregorianCalendar(1990, Calendar.JUNE, 15).getTime());
+
+        DataSource adapter = new InfoAdapter(info);
+        UserData userData = new UserData();
+        userData.getData(adapter);
+
+    }
 }
